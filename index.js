c0827c8c3f79147013772cdba7f5d76c1c077267
Vincent Le Quang <vincent@thumbtack.com>
2018-06-05 22:59:37 -0700

    Test child process

--- index.js
+++ index.js
@@ -4,10 +4,16 @@ const PORT = process.env.PORT || 5000
 
 const child_process = require('child_process')
 
-app.get('/', (req, res) => {
+app.get('/', (req, res) => {  
    res.send('Hello ' + process.platform);
 });
 
 app.listen(PORT, () => {
    console.log('Test ' + PORT);
 });
+
+
+   exec('ls', function(err, data) {
+        console.log(err)
+        console.log(data.toString());
+   });
