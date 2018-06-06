968c8ba18958c1b81cc30a58c50a42d48c41d842
Vincent Le Quang <vincent@thumbtack.com>
2018-06-05 22:57:13 -0700

    Add child process

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 5000
 
+const child_process = require('child_process')
+
 app.get('/', (req, res) => {
    res.send('Hello ' + process.platform);
 });
