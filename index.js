bf01e06f584965fb3506d20d185ce48e10bca571
Vincent Le Quang <vincent@thumbtack.com>
2018-06-05 23:03:58 -0700

    Fix child_process

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.listen(PORT, () => {
 });
 
 
-   exec('ls', function(err, data) {
+child_process.exec('ls', function(err, data) {
         console.log(err)
         console.log(data.toString());
    });
