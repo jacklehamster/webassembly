ba6adf5f79073a90534546c1045b9e8868bd26a9
Vincent Le Quang <vincent@thumbtack.com>
2018-06-06 09:01:34 -0700

    Test Webassembly call

--- index.js
+++ index.js
@@ -5,31 +5,57 @@ const PORT = process.env.PORT || 5000
 const child_process = require('child_process')
 
 app.get('/', (req, res) => {  
-   res.send('Hello ' + process.platform);
+
+   PostCode(`void sum(int a, int b) {
+     return a + b;
+   }`, chunk => {
+      res.send('Hello ' + process.platform);
+      res.send(chunk);
+   });
+
+
+
 });
 
 app.listen(PORT, () => {
    console.log('Test ' + PORT);
 });
 
-child_process.exec('ls', function(err, data) {
-        console.log(err)
-        console.log(data.toString());
-child_process.exec('./emsdk-portable/emsdk update', function(err, data) {
-        console.log(err)
-        console.log(data.toString());
-        child_process.exec('./emsdk-portable/emsdk install latest', function(err, data) {
-        console.log(err)
-        console.log(data.toString());
-             child_process.exec('./emsdk-portable/emsdk activate latest', function(err, data) {
-        console.log(err)
-        console.log(data.toString());
-child_process.exec('. ./emsdk-portable/emsdk_env.sh', function(err, data) {
-            console.log(err)
-        console.log(data.toString());
-});
-             });
-        });
 
-   });
-});
+// We need this to build our post string
+const querystring = require('querystring');
+const http = require('http');
+const fs = require('fs');
+
+function PostCode(codestring, callback) {
+  // Build the post string from an object
+  var post_data = querystring.stringify({
+      'input' : codestring,
+      'action': 'cpp2wast',
+      'output_info': 'compiled_code',
+      'options' : '-std=c++11 -Os',
+  });
+
+  // An object of options to indicate where to post to
+  var post_options = {
+      host: 'https://wasmexplorer-service.herokuapp.com',
+      port: '80',
+      path: '/service.php',
+      method: 'POST',
+      headers: {
+          'Content-Type': 'application/x-www-form-urlencoded',
+          'Content-Length': Buffer.byteLength(post_data)
+      }
+  };
+
+  // Set up the request
+  var post_req = http.request(post_options, function(res) {
+      res.setEncoding('utf8');
+      res.on('data', callback);
+  });
+
+  // post the data
+  post_req.write(post_data);
+  post_req.end();
+
+}
