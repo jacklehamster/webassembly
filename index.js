e5faede48c624dc180d9f275f010a911e2ce3a0f
Vincent Le Quang <vincent@thumbtack.com>
2018-06-05 22:31:46 -0700

    Update port

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const app = express();
+const PORT = process.env.PORT || 5000
 
 app.get('/', (req, res) => {
    res.send('Hello');
 });
 
-app.listen(5000, () => {
-   console.log('Test 5000');
+app.listen(PORT, () => {
+   console.log('Test ' + PORT);
 });
