6d33993c07344d304e49c8d107d4e86c8dc438b3
Vincent Le Quang <vincent@thumbtack.com>
2018-06-06 19:13:24 -0700

    All origin

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 const express = require('express');
+const child_process = require('child_process')
+const cors = require('cors');
+
 const app = express();
 const PORT = process.env.PORT || 5000
 
-const child_process = require('child_process')
+
+
+app.use(cors({origin: '*'}));
 
 app.get('/compile', (req, res) => {  
 
